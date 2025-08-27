c57d3c0640c476abfcb5172a9d61f1aed3c58b80
Moksina-Akter <msdiya01865@gmail.com>
2025-08-27 12:19:38 +0600

    main section added

---
+++ index.js
@@ -0,0 +1,14 @@
+
+
+/*
+*appent tag:
+    <div class="bg-[#fafafa] rounded-md p-5 mt-3">
+        <h1 class="inter font-semibold">Fire Service Number</h1>
+        <div class="hind-madurai flex justify-between text-[#5c5c5c]">
+            <h4 >999</h4>
+            <p>11:36:58 AM</p>           
+        </div>
+    </div>
+
+*/ 
+                     
