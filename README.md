0bbff99cb42cac02f81322bbf919ce17c61c54a0
Moksina-Akter <msdiya01865@gmail.com>
2025-08-28 14:34:44 +0600

    js added

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
-# Ph-Assignment-05
+
+### JavaScript DOM Questions & Answers :-
+
+
+# What is the difference between getElementById, getElementsByClassName, and querySelector / querySelectorAll?
+## Answer of question 1:-
+The propertygetElementById** selects a single element by its id. It returns the element object or null if not found. And **getElementByClassName** selects all elements with a same class. Reans can select multiple items and return a live HTML collection. And the property **querySelector* and *querySelectorAll** can select elements using ans CSS selector. The **querySelector** selects the first matching element and **querySelectorAll selects Nodelist of all matching elemnets.
+
+# How do you create and insert a new element into the DOM?
+## Answer of question 2:-
+I will use **document.createElement()** to create an element and then I will use appendChild()** to insert the new created element in DOM.
+
+# What is Event Bubbling and how does it work?
+## Answer of question 3:
+**Event Bubbling is a procces in DOM where an event first occurs on the child and then propagates upword through it's parents. The button's listener executes first and then the container's listener executes. Means the event happens on the target element and then the event moves upwords to the parent element.
+
+# What is Event Delegation in JavaScript? Why is it useful?
+## Answer of question 4:-
+**Event Delegation is a technique in JavaScript where attach a single event listener to a parent element instead of adding multiple separate listeners to the child elements. It is useful because by using this we can add just one listener on the parent instead of adding many separate listeners on the child elements. It also works for dynamic elements, we don't need to add a listener every time when a new element is created.
+
+# What is the difference between preventDefault() and stopPropagation() methods?
+## Answer of question 5:-
+The **preventDefault()** stops the default action of an element from happening. It doesn't stop the event from bubbling up. On the other hand, the stopPropagation()** method stops the event from bubbling up to perent elements. It does not stop the default action of an element.
--- index.js
+++ index.js
@@ -1,5 +1,94 @@
 
 
+function getElement(id) {
+  const element = document.getElementById(id);
+  return element;
+}
+
+// heart count
+let count = 0;
+const countDisplay = document.querySelectorAll('.heartClick');
+for (const counts of countDisplay) {
+     counts.addEventListener('click', function (){
+    count++;
+    getElement('heartCount').innerText = count;
+})
+}
+
+
+// copy count
+const copy = document.querySelectorAll('.copyBtn');
+for (const copies of copy) {
+     copies.addEventListener('click', function (){
+    count++;
+    getElement('copied').innerText = count;
+})
+}
+
+
+//calling count 
+/*
+const calls = document.querySelectorAll('.callBtn');
+for (const call of calls) {
+    call.addEventListener('click', function (){
+     
+        const cardHeading =document.getElementsByClassName('cardHeading')[0].innerText;
+        const cardNumber =document.getElementsByClassName('number')[0].innerText;
+
+
+        console.log(cardHeading,cardNumber)
+        alert(`Calling ${cardHeading} ${cardNumber}`);
+
+    })
+}
+*/
+
+
+const calls = document.querySelectorAll('.callBtn');
+
+for (const call of calls) {
+  call.addEventListener('click', function () {
+    const card = call.closest('.card'); 
+    const cardHeading = card.getElementsByClassName('cardHeading')[0].innerText;
+    const cardNumber = card.getElementsByClassName('number')[0].innerText;
+
+    alert(`Calling ${cardHeading} ${cardNumber}`);
+
+    const starElement = getElement('starDecrease');
+    let starValue = parseInt(starElement.innerText);
+    starValue = starValue - 20 ;
+
+     if (starValue < 0) starValue = 0;
+        starElement.innerText = starValue;
+
+
+      const time = new Date ();  
+      const hours = time.getHours()
+      const minutes = time.getMinutes()
+      const seconds = time.getSeconds()
+      const currentTime = `${hours} : ${minutes}:${seconds}`
+      
+
+      const div = getElement('card-container');
+      const newDiv = document.createElement('div') ;
+      newDiv.innerHTML = `
+      <div class="bg-[#fafafa] rounded-md p-5 mt-3">
+        <h1 class="inter font-semibold">${cardHeading}</h1>
+        <div class="hind-madurai flex justify-between text-[#5c5c5c]">
+            <h4 >${cardNumber}</h4>
+            <p>${currentTime}</p>           
+        </div>
+      </div>
+      
+      `;
+      div.append(newDiv);
+      
+
+  });
+};
+
+
+
 /*
 *appent tag:
     <div class="bg-[#fafafa] rounded-md p-5 mt-3">
